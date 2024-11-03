00496a5cc83a6b6b2472dfbc68a580515befb6cb
dereje-boy <derejeg35@gmail.com>
2024-11-03 11:40:11 +0300

    to check prisma

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 
 const express = require('express');
+const body_parser = require('body-parser');
+
+const {PrismaClient} = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 const app = express();
 
+app.use(body_parser.urlencoded({ extended: true }))
+
+app.use(body_parser.raw({ extended: true }))
+
+app.use(body_parser.json({ extended: true }))
+
 
 app.get('/', (req, res)=>{
-	res.send("Welcome home");
-	});
+    res.send("Welcome home");
+});
+
+app.post("/", (req,res)=>{
+    console.log(req.body);
+    res.json(req.body);
+});
 
 app.listen(3000, ()=>console.log("Server started successfully on 3000 port "));
